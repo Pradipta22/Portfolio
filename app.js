c99503ac9181228b77c957cb722ff00033d5f4cc
Pradipta <81964254+Pradipta22@users.noreply.github.com>
2022-11-29 12:37:42 +0530

    newfile
    new file

---
+++ app.js
@@ -0,0 +1,23 @@
+$(function() {
+    $('.chart').easyPieChart({
+      size: 160,
+      barColor: "#36e617",
+      scaleLength: 0,
+      lineWidth: 15,
+      trackColor: "#525151",
+      lineCap: "circle",
+      animate: 2000,
+    });
+  });
+  
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz6r2Mh1Mr-Z4gomSLROPGKCz_XNtBkje6jYVCQ9jQZ2g2jTJJm5QSP3sMsK_LAsnC6/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+ ;
+ 
